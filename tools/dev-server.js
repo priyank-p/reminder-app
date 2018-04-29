215b618ef0f68bc5d0dd2d1dce2521230274a378
Priyank Patel <priyankp390@gmail.com>
2018-04-29 23:55:48 +0000

    tools: Add webpack config and tools.

---
+++ tools/dev-server.js
@@ -0,0 +1,26 @@
+const { spawn } = require('child_process');
+const path = require('path');
+
+const devProcesses = new Map();
+const spawnOpts = {
+  cwd: path.resolve(__dirname, '..'),
+  env: process.env,
+  stdio: 'inherit',
+  detached: true
+};
+
+const webpackDevServer = './node_modules/.bin/webpack-dev-server';
+const webpackProcess = spawn(webpackDevServer, ['--config', 'tools/webpack.config.js'], spawnOpts);
+const expressServer = spawn('node', ['server', '--dev'], spawnOpts);
+
+devProcesses.set('webpack', webpackProcess);
+devProcesses.set('express-server', expressServer);
+
+function killProcesses() {
+  devProcesses.forEach((proc, name) => {
+    process.kill(-proc.pid);
+  });
+}
+
+process.on('SIGINT', killProcesses);
+process.on('SIGTERM', killProcesses);
---
+++ tools/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const BundleTracker = require('webpack-bundle-tracker');
+
+const ROOT_DIR = path.resolve(__dirname, '..');
+const port = +process.env.PORT + 1 || 7322;
+const host = process.env.HOST || 'localhost';
+const { ALLOWED_HOSTS } = process.env;
+const allowedHosts = ALLOWED_HOSTS ? [ALLOWED_HOSTS] : undefined;
+
+module.exports = (env) => {
+  const production = env === 'production';
+  const config = {
+    mode: production ? "production" : "development",
+    context: ROOT_DIR,
+    entry: {
+      'main': './static/js/index.js'
+    },
+    plugins: [
+        new BundleTracker({
+          path: path.join(ROOT_DIR, 'var'),
+          filename: 'webpack-bundles.json'
+        })
+      ],
+    output: {
+      publicPath: '/webpack-bundles/',
+      path: path.join(ROOT_DIR, '/static/webpack-bundles'),
+      filename: production ? '[name]-[hash].js' : '[name].js',
+    },
+    devtool: production ? 'source-map' : 'cheap-module-eval-source-map'
+  };
+
+  if (!production) {
+    config.output.publicPath = '/webpack/';
+    config.devServer = {
+      historyApiFallback: true,
+      compress: true,
+      overlay: true,
+      allowedHosts,
+      host,
+      port
+    };
+  }
+
+  return config;
+};
