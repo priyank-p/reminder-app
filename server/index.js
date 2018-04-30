4b0b983fc3c12fe22671b27b54d9908c5b9ddceb
Priyank Patel <priyankp390@gmail.com>
2018-04-30 00:36:13 +0000

    server: only setup proxy to webpack-dev-server in dev env.

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const proxy = require('http-proxy-middleware');
 const settings = require('./settings');
 const views = require('./views');
 
+const devlopment = process.argv[2] == 'dev';
 const app = express();
 settings(app);
 views(app);
 
-// starts app
 const port = process.env.PORT || 7213;
-const host = process.env.HOST || 'localhost';
-app.use('/webpack', proxy({
-  target: `http://${host}:${+port + 1}`,
-  changeOrigin: true
-}));
-
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
 });
+
+// if devlopment redirect /webpack to webpack-dev-server
+if (devlopment) {
+  const proxy = require('http-proxy-middleware');
+  const host = process.env.HOST || 'localhost';
+
+  app.use('/webpack', proxy({
+    target: `http://${host}:${+port + 1}`,
+    changeOrigin: true
+  }));
+}
