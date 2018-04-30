7ccfaefeaa8b78dba0819f4e285cbe30e59fc951
Priyank Patel <priyankp390@gmail.com>
2018-04-30 00:21:02 +0000

    server: minify html being sent to user.

--- server/index.js
+++ server/index.js
@@ -2,13 +2,30 @@ const path = require('path');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const proxy = require('http-proxy-middleware');
+const minifyHTML = require('express-minify-html');
+
+const ROOT_DIR = path.resolve(__dirname, '..');
+process.chdir(ROOT_DIR);
 
 const app = express();
-app.set('views', path.join(__dirname, 'static/templates'));
+app.set('views', path.join(ROOT_DIR, 'static/templates'));
 app.engine('hbs', handlebars({
   extname: '.hbs',
   partialsDir: 'static/templates/partials',
   helpers: {}
+}));
+
+app.use(minifyHTML({
+  override: true,
+  exception_url: false,
+  htmlMinifier: {
+    removeComments: true,
+    collapseWhitespace: true,
+    collapseBooleanAttributes: true,
+    removeAttributeQuotes: true,
+    removeEmptyAttributes: true,
+    minifyJS: true
+  }
 }));
 
 app.set('view engine', 'hbs');
