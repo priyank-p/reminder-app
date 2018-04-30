1aa0a10455452c6f444756f6ec4e24fc53853840
Priyank Patel <priyankp390@gmail.com>
2018-04-30 16:47:10 +0000

    tests: remove mocha and add some more tests.

--- test/handlebars-helpers.js
+++ test/handlebars-helpers.js
@@ -1,45 +1,71 @@
 const assert = require('assert');
 const path = require('path');
-const fs = require('fs');
-const _fs = Object.assign({},fs);
 
 const {
   webpack_dev_bundle,
   webpack_prod_bundle
-} = require('./fixtures');
+} = fixtures;
 
-let currentBundle = webpack_dev_bundle;
-const bundleFile = path.resolve(__dirname, '../var/webpack-bundles.json');
-fs.readFileSync = (...args) => {
-  if (args[0] !== bundleFile) {
-    return _fs.readFileSync(...args);
-  }
+const handlebarsHelpers = require('../server/handlebars-helpers');
+const { render_bundle } = handlebarsHelpers;
+handlebarsHelpers.__updateBundle(webpack_dev_bundle);
 
-  return JSON.stringify(currentBundle);
-};
-
-let { render_bundle } = require('../server/handlebars-helpers')
-describe('handlebars-helpers tests', () => {
-  describe('render_bundle tests', () => {
-    it('throws error is bundle does not exsist', () => {
-      assert.throws(() => {
-        render_bundle('EEXIST');
-      });
-    });
-
-    it('renders bundles correctly', () => {
-      const expected = {
-        js: `<script src="/webpack/main.js" >`,
-        css: `<link rel="stylesheet" href="/webpack/styles.css" >`
-      };
-
-      const actual = {
-        js: render_bundle('main'),
-        css: render_bundle('styles')
-      };
-
-      assert.deepStrictEqual(expected['js'], actual['js']);
-      assert.deepStrictEqual(expected['css'], actual['css']);
-    });
+(function test_rendering_wrong_bundle() {
+  assert.throws(() => {
+    render_bundle('EEXIST');
   });
-});
+})();
+
+(function test_correct_html_rendering() {
+  const expected = {
+    js: '<script src="/webpack/main.js" >\n',
+    css: '<link rel="stylesheet" href="/webpack/styles.css" >\n'
+  };
+
+  const actual = {
+    js: render_bundle('main'),
+    css: render_bundle('styles')
+  };
+
+  assert.deepStrictEqual(expected['js'], actual['js']);
+  assert.deepStrictEqual(expected['css'], actual['css']);
+})();
+
+(function test_adding_attributes_to_render_bundle() {
+  const expected = {
+    js: '<script src="/webpack/main.js" data-js="main-bundle">\n',
+    css: '<link rel="stylesheet" href="/webpack/styles.css" data-css="css-bundle">\n'
+  };
+
+  const actual = {
+    js: render_bundle('main', 'data-js="main-bundle"'),
+    css: render_bundle('styles', 'data-css="css-bundle"')
+  };
+
+  assert.deepStrictEqual(expected['js'], actual['js']);
+  assert.deepStrictEqual(expected['css'], actual['css']);
+})();
+
+handlebarsHelpers.__updateBundle(webpack_prod_bundle);
+(function test_render_bundle_for_multiple_file_in_bundle() {
+  /*
+    Test weather the render_bundle handles multip files in one
+    entry so two cases:
+      - one the map files generated in prod build
+      - second multip files to render in one bundle
+        i.e js and css in one bundle.
+  */
+  const expected = {
+    js: '<script src="/webpack-bundles/main-122cd28c64172654bd14.js" >\n',
+    css: '<script src="/webpack-bundles/reminder-app-bd23999de3211229320.js" >\n'
+          + '<link rel="stylesheet" href="/webpack-bundles/styles-bd23999de3211229320.css" >\n'
+  };
+
+  const actual = {
+    js: render_bundle('main'),
+    css: render_bundle('app')
+  };
+
+  assert.deepStrictEqual(expected['js'], actual['js']);
+  assert.deepStrictEqual(expected['css'], actual['css']);
+})();
