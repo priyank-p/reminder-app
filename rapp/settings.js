6608b15b4285c7926fafe7912edb9b1c898372ec
Priyank Patel <priyankp390@gmail.com>
2018-04-30 19:52:08 +0000

    render_bundle: fix broken script tag in helper.

--- rapp/settings.js
+++ rapp/settings.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const handlebars = require('express-handlebars');
 const minifyHTML = require('express-minify-html');
+const handlebarsHelpers = require('./handlebars-helpers')
 
 const ROOT_DIR = path.resolve(__dirname, '..');
 module.exports = (app) => {
   app.set('views', path.join(ROOT_DIR, 'static/templates'));
-  app.engine('hbs', handlebars({
+  app.engine('hbs',  handlebars({
     extname: '.hbs',
     partialsDir: 'static/templates/partials',
-    helpers: {}
+    helpers: handlebarsHelpers
   }));
 
   app.use(minifyHTML({
