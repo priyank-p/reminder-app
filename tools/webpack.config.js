3a32853a80717ffb783f23c4b1d13b861f3262b4
Priyank Patel <priyankp390@gmail.com>
2018-05-03 23:54:01 +0000

    webpack: add support scss files.

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const BundleTracker = require('webpack-bundle-tracker');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const ROOT_DIR = path.resolve(__dirname, '..');
 const port = +process.env.PORT + 1 || 7322;
@@ -7,13 +8,24 @@ const host = process.env.HOST || 'localhost';
 const { ALLOWED_HOSTS } = process.env;
 const allowedHosts = ALLOWED_HOSTS ? [ALLOWED_HOSTS] : undefined;
 
+function cssLoaders(isProd, bundles) {
+  if (!isProd) {
+    bundles.unshift('css-hot-loader');
+  }
+
+  return bundles;
+}
+
 module.exports = (env) => {
   const production = env === 'production';
   const config = {
     mode: production ? "production" : "development",
     context: ROOT_DIR,
     entry: {
-      'main': './static/js/index.js'
+      'main': [
+          './static/js/index.js',
+          './static/scss/reminder-app.scss'
+        ]
     },
     plugins: [
         new BundleTracker({
@@ -26,10 +38,49 @@ module.exports = (env) => {
       path: path.join(ROOT_DIR, '/static/webpack-bundles'),
       filename: production ? '[name]-[hash].js' : '[name].js',
     },
+    module: {
+      rules: [
+        {
+          test: /\.(sass|scss)$/,
+          use: cssLoaders(production, [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                  sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                  sourceMap: true
+              }
+            }
+          ]),
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".js", ".json", ".scss", ".css"],
+    },
     devtool: production ? 'source-map' : 'cheap-module-eval-source-map'
   };
 
-  if (!production) {
+  if (production) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name]-[contenthash].css",
+        chunkFilename: "[id].css"
+      })
+    );
+  } else {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    );
+
     config.output.publicPath = '/webpack/';
     config.devServer = {
       historyApiFallback: true,
