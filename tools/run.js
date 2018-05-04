2a5fb13fed9be4dd57a4ad8b4f97a0dc9b738578
Priyank Patel <priyankp390@gmail.com>
2018-05-04 00:13:01 +0000

    tools: add run.js file.

---
+++ tools/run.js
@@ -0,0 +1,34 @@
+const { spawn } = require('child_process');
+
+const defaultOpts = {
+  cwd: process.cwd(),
+  env: process.env,
+  stdio: 'inherit'
+};
+
+function run(commandToRun, passedOpts) {
+  const args = commandToRun.split(' ');
+  const cmd = args[0];
+  args.splice(0, 1);
+
+  console.log('+', commandToRun);
+  const opts = { ...defaultOpts, ...passedOpts };
+  const proc = spawn(cmd, args, opts);
+
+  return new Promise((resolve, reject) => {
+    function check(code) {
+      if (code != 0) {
+        reject();
+        return;
+      }
+
+      resolve();
+    }
+
+    proc.on('exit', check);
+    proc.on('close', check);
+    proc.on('error', reject);
+  });
+}
+
+module.exports = run;
