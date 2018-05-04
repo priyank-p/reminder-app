5875451eaaf50f13bc885aebbc8414fc7b3f4015
Priyank Patel <priyankp390@gmail.com>
2018-05-04 14:46:11 +0000

    settings: setup header for all requests.

--- app/settings.js
+++ app/settings.js
@@ -4,6 +4,7 @@ const minifyHTML = require('express-minify-html');
 const handlebarsHelpers = require('./handlebars-helpers');
 
 const ROOT_DIR = path.resolve(__dirname, '..');
+const production = process.env.RAPP_PRODUCTION === 'true';
 module.exports = (app) => {
   app.set('views', path.join(ROOT_DIR, 'static/templates'));
   app.engine('hbs',  handlebars({
@@ -26,4 +27,26 @@ module.exports = (app) => {
   }));
 
   app.set('view engine', 'hbs');
+
+  // Configure headers
+  app.use(function (req, res, next) {
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+
+    // add cache-control no-cache to html files
+    const isHTMLRequest = req.path.endsWith('/') || req.path.includes('.html');
+    if (isHTMLRequest) {
+      res.setHeader('Cache-Control', 'no-cache, must-revalidate')
+    }
+
+    if (production) {
+      res.setHeader('Strict-Transport-Security', 'max-age=31536000');
+
+      if (req.path.includes('/static/')) {
+        res.setHeader('Cache-Control', 'public, max-age=31536000');
+      }
+    }
+
+    next();
+  });
 };
