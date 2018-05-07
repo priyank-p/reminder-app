2c396c252fe9f1bfa81d879d87c6bdcd8c65a9cb
Priyank Patel <priyankp390@gmail.com>
2018-05-07 11:37:55 +0000

    app: add inital add reminder modal.

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,17 @@
 const { $ } = require('./dom');
-const { addHotkeys } = require('./hotkeys');
+const { addHotkey } = require('./hotkeys');
+
+const reminderModal = $('#add-reminder-modal');
+const addReminderBtn = $('#add-action-btn');
+function toogleReminderModal() {
+  reminderModal.classList.toggle('open');
+}
+
+addHotkey('a', toogleReminderModal);
+addReminderBtn.addEventListener('click', toogleReminderModal);
+reminderModal.addEventListener('click', (e) => {
+  // if the modal was clicked and not the form
+  if (e.target === e.currentTarget) {
+    toogleReminderModal();
+  }
+});
