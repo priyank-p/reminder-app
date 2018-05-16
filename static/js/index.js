7dad1064fc60cfe3cfeaed1d971ec704927df2d1
Priyank Patel <priyankp390@gmail.com>
2018-05-16 16:26:13 +0000

    reminder-app: add input for chossing due date and time.

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,3 @@
-const { $ } = require('./dom');
-
-const dueDateInput = $('#add-reminder-modal form input[type=date]');
-const date = new Date();
-const tommorow = new Date(date.getTime() + 86400000);
-const month = tommorow.getMonth() + 1;
-const nextDate = tommorow.getDate() + 1;
-let inputValue = `${tommorow.getFullYear()}-`;
-inputValue += month < 10 ? '0' + month : month;
-inputValue += '-';
-inputValue += nextDate < 10 ? '0' + nextDate : nextDate;
-dueDateInput.value = inputValue;
-
-const dropdowns = $('.dropdown', 'nodelist');
-
 // require event handlers
 require('./app-hotkeys');
 require('./event-listeners');
