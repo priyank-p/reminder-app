eba9fe3454cb48bd79f36b12fe73099c656c64a3
Priyank Patel <priyankp390@gmail.com>
2018-05-24 15:58:09 +0000

    routes: add post route for adding reminder.

--- app/routes/app-routes.js
+++ app/routes/app-routes.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
+});
+
+router.post('/', (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    res.status(500);
+    res.send('Reminder must be passed in!');
+    return;
+  }
+
+  console.log(req.body);
+  res.status(200);
 });
 
 module.exports = router;
