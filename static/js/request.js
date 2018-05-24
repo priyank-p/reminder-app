f170076d17c14b02485bf1107f5fdc69661fceb5
Priyank Patel <priyankp390@gmail.com>
2018-05-24 15:59:04 +0000

    js: add request module for handling get and post async request.

---
+++ static/js/request.js
@@ -0,0 +1,30 @@
+function get(url, headers = {}) {
+  const request = new Request(url, {
+    method: 'GET',
+    headers: new Headers(headers)
+  });
+
+  return fetch(request);
+}
+
+const defaultPostHeaders = {
+  'Content-Type': 'application/json'
+};
+
+function post(url, body, _headers = {}) {
+  const headers = { ...defaultPostHeaders, ..._headers };
+  const request = new Request(url, {
+    method: 'POST',
+    headers: new Headers(headers),
+    body: JSON.stringify(body)
+  });
+
+  return fetch(request);
+}
+
+module.exports = {
+  post,
+  get
+};
+
+window.request = module.exports;
