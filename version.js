757dcb7a6f728830f2e212068faf1977033b6af4
Priyank Patel <priyankp390@gmail.com>
2018-05-25 15:52:46 +0000

    version: add version.js file to keep track of versions.
    This for now intiall stores MIGRATION_VERSION.

---
+++ version.js
@@ -0,0 +1,11 @@
+/** VERSIONFILE
+  Version file to keep track of version required through
+  the app.
+*/
+
+// Bump this when new database migration is added!
+const MIGRATION_VERSION = '01';
+
+module.exports = {
+  MIGRATION_VERSION
+};
