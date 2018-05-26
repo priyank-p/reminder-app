57cd4e296936cbda87eea08bb0b2e2358740cd44
Priyank Patel <priyankp390@gmail.com>
2018-05-26 15:45:37 +0000

    routes: api routes add endpoint to delete a reminder.

--- app/routes/api.js
+++ app/routes/api.js
@@ -13,7 +13,7 @@ router.post('/reminders/add', async (req, res) => {
   if (reminder.due_date) {
     reminder.due_date = new Date(reminder.due_date);
   }
-  
+
   await reminders.addReminder(reminder);
   res.send('Reminder Added!');
 });
@@ -21,6 +21,24 @@ router.post('/reminders/add', async (req, res) => {
 router.get('/reminders/all', async (req, res) => {
   const allReminders = await reminders.getReminders();
   res.send(JSON.stringify(allReminders));
+});
+
+router.delete('/reminders/delete/:id', async (req, res) => {
+  const id = +req.params.id;
+  if (isNaN(id)) {
+    res.status(500);
+    res.send('Reminder id to delete must be passed in, must be valid integer');
+    return;
+  }
+
+  reminders.deleteReminder(id)
+    .then(() => {
+      res.send(`Reminder with id: ${id} was deleted!`);
+    })
+    .catch(() => {
+      res.status(500);
+      res.send(`Cannot delete reminder with id: ${id} that is not added!`);
+    });
 });
 
 module.exports = router;
