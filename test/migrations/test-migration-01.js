a7b69cd4cc983d8605c67297795b6bb4daa9befb
Priyank Patel <priyankp390@gmail.com>
2018-05-29 12:41:34 +0000

    migrations-tests: add test for 01-migration.

---
+++ test/migrations/test-migration-01.js
@@ -0,0 +1,25 @@
+async function prep() {
+  await migrations.run_migration(0);
+  await migrations.run_migration(1);
+}
+
+async function test_migration_02() {
+  const { db } = env['reminder-db'];
+  const levelDB = db.db;
+
+  const InternalProps = await levelDB.get('__InternalProps');
+  const reminderTable = InternalProps.tables.reminders;
+  for (let field in reminderTable) {
+    if (field === 'ids' || field === 'create_at') {
+      continue;
+    }
+
+    const fieldInfo = reminderTable[field];
+    assert(fieldInfo.required);
+  }
+}
+
+module.exports = async function() {
+  await prep();
+  await test_migration_02();
+};
