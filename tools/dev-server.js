90d85cbc4507a09214a99403036781420d729a4b
Priyank Patel <priyankp390@gmail.com>
2018-05-30 23:23:48 +0000

    dev-server: add --webpack-ouput flag to enable webpack output.

--- tools/dev-server.js
+++ tools/dev-server.js
@@ -9,8 +9,23 @@ const spawnOpts = {
   detached: true
 };
 
+const argparser = require('../app/argparser');
+const args = argparser(`
+Runs the reminder-app development server.
+
+Flags:
+----------------
+--webpack-output    Enables webpack output for dev-server, which is disabled by default.
+`);
+args.add('--webpack-output', { type: 'boolean' });
+args.parse();
+
 const webpackDevServer = './node_modules/.bin/webpack-dev-server';
-const webpackArgs = ['--config', 'tools/webpack.config.js', '--hot', '--quiet'];
+const webpackArgs = ['--config', 'tools/webpack.config.js', '--hot'];
+if (!args['webpack-output']) {
+  webpackArgs.push('--quiet');
+}
+
 const webpackProcess = spawn(webpackDevServer, webpackArgs, spawnOpts);
 const expressServer = spawn('node', ['app', '--dev'], spawnOpts);
 
