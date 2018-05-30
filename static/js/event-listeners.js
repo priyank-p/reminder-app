e6e5a2692a01ff67b2177165ea17c215069e5060
Priyank Patel <priyankp390@gmail.com>
2018-05-30 13:33:27 +0000

    app: add quick due date picker.
    This allows the user to enter a day and auto increments the date for them.

--- static/js/event-listeners.js
+++ static/js/event-listeners.js
@@ -1,5 +1,6 @@
 const { $ } = require('./dom');
 const request = require('./request');
+const dateFormat = require('dateformat');
 
 const reminderModal = $('#add-reminder-modal');
 const addReminderBtn = $('#add-reminder-btn');
@@ -45,4 +46,45 @@ reminderForm.addEventListener('submit', function (e) {
     });
   toogleReminderModal();
   e.preventDefault();
+});
+
+
+// set the due date of the .quick-choose -
+// the quick due date picker, we get a number
+// and we update the due date fields, the due_time
+// would be by default 12 PM
+const inputBaseSelector = '#add-reminder-modal form';
+const dueDateInput = $(`${inputBaseSelector} input[type="date"]`);
+const dueTimeInput = $(`${inputBaseSelector} input[type="time"]`);
+function setDueDate(n) {
+  if (isNaN(n)) {
+    return;
+  }
+
+  // its possible to enter negative number despite
+  // the the input min/max attributes...
+  // to avoid infinite loop
+  if (n < 0) {
+    return;
+  }
+
+  // this is required because even though
+  // Date object is smart about month rollover
+  // it can't increment 10 days from last to second day
+  const date = new Date();
+  for (let i = 0; i < n; i++) {
+    date.setDate(date.getDate() + 1);
+  }
+
+  // the value muse be in standard yyyy-mm-dd format
+  dueDateInput.value = dateFormat(date, 'yyyy-mm-dd');
+  if (dueTimeInput.value === '') {
+    dueTimeInput.value = '12:00';
+  }
+}
+
+const quickDueDatePicker = $('.quick-choose input');
+quickDueDatePicker.addEventListener('input', (e) => {
+  const el = e.target;
+  setDueDate(el.value);
 });
