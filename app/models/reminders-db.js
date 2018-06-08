34aadfc36ad752536f73b8c8e55f742d643e63bc
Priyank Patel <priyankp390@gmail.com>
2018-06-08 17:04:47 +0000

    models: use the table instance of reminders.

--- app/models/reminders-db.js
+++ app/models/reminders-db.js
@@ -7,11 +7,17 @@ const DB_NAME = production ? 'reminder-app' : `reminder-app-${env.mode}`;
 const DB_PATH = path.resolve(__dirname, '../../var/', DB_NAME);
 const db = new Uplevel(DB_PATH);
 
+// we don't use the async method here,
+// since we know the table is created in advance
+const reminders = new Uplevel.UplevelTableInstance(db, 'reminders');
+
 module.exports = {
   tableName: 'reminders',
+  reminders,
   db
 };
 
+// TODO: Move away from using db.
 if (env.tests) {
   env.setEnv('reminder-db', module.exports);
 }
