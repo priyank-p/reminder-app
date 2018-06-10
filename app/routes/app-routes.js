82a8bc95aa6491323b8521d9765c4dc7f43f0752
Priyank Patel <priyankp390@gmail.com>
2018-06-10 23:34:40 +0000

    app-server: send push notification public key.

--- app/routes/app-routes.js
+++ app/routes/app-routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const reminders = require('../models/reminders');
+const pushNotifications = require('../push-notifications');
 
+const { webPushData } = pushNotifications;
 const router = express.Router();
+
 router.get('/', async (req, res) => {
   res.render('index', {
-    reminders: await reminders.getReminders()
+    reminders: await reminders.getReminders(),
+    publicKey: webPushData.publicKey
   });
 });
 
