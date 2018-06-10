2f7c0fe6442e1aca9b2087ce02c5d3ff0170f809
Priyank Patel <priyankp390@gmail.com>
2018-06-10 16:28:08 +0000

    routes: add update reminder api route.

--- app/routes/api.js
+++ app/routes/api.js
@@ -41,4 +41,29 @@ router.delete('/reminders/delete/:id', async (req, res) => {
     });
 });
 
+router.post('/reminders/update/:id', async (req, res) => {
+  const id = +req.params.id;
+  if (isNaN(id)) {
+    res.status(500);
+    res.send('Reminder id to delete must be passed in, and must be valid integer');
+    return;
+  }
+
+  if (Object.keys(req.body).length !== 2) {
+    res.status(500);
+    res.send('Reminder must be passed in!');
+    return;
+  }
+
+
+  reminders.updateReminder(id, req.body)
+    .then(() => {
+      res.send(`Reminder with id: ${id} was updated!`);
+    })
+    .catch(() => {
+      res.status(500);
+      res.send(`Cannot updated reminder with id: ${id}!`);
+    });
+});
+
 module.exports = router;
