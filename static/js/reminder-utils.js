d99e7bd16133d32348524ff31ab7dfc683875b74
Priyank Patel <priyankp390@gmail.com>
2018-06-11 16:46:09 +0000

    app: fix wrong date being sent to backened.

--- static/js/reminder-utils.js
+++ static/js/reminder-utils.js
@@ -7,11 +7,18 @@ export function getReminder(el, { isEditingUI }) {
     reminder[input.name] = input.value;
   });
 
-  if (reminder.due_date) {
-    const time = reminder.due_time ? 'T' + reminder.due_time : '';
-    reminder.due_date = new Date(reminder.due_date + time);
+  reminder.due_date = new Date(reminder.due_date);
+  if (reminder.due_time) {
+    const [ hour, minutes ] = reminder.due_time.split(':').map(_ => parseInt(_));
+    const dueDate = reminder.due_date;
+
+    dueDate.setHours(hour, minutes, 0);
+    reminder.due_date = dueDate;
   }
 
+  // don't tranform date to json/iso/utc it does work the same in
+  // backend, meaning the date is changed in backend.
+  reminder.due_date = reminder.due_date.toLocaleString();
   delete reminder.due_time;
   return reminder;
 }
