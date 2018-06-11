46ff33993a3ef4efdac86ccde192b66c1c0fb2a8
Priyank Patel <priyankp390@gmail.com>
2018-06-11 22:24:04 +0000

    service-worker: add code for show push notifications.

--- static/js/reminder-app-sw.js
+++ static/js/reminder-app-sw.js
@@ -5,3 +5,15 @@ self.addEventListener('install', function() {
 self.addEventListener('activate', function (event) {
   event.waitUntil(self.clients.claim());
 });
+
+self.addEventListener('push', function(event) {
+  console.log(event);
+  const { body, title, tag } = event.json();
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      tag: tag
+    })
+  );
+});
