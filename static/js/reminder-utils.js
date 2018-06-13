bcfebc1636b9511a8d331ef779209f7dfc7dacea
Priyank Patel <priyankp390@gmail.com>
2018-06-13 14:53:02 +0000

    reminder-utils: fix a glitch with wrong time being sent to backend.

--- static/js/reminder-utils.js
+++ static/js/reminder-utils.js
@@ -7,7 +7,7 @@ export function getReminder(el, { isEditingUI }) {
     reminder[input.name] = input.value;
   });
 
-  reminder.due_date = new Date(reminder.due_date);
+  reminder.due_date = new Date(reminder.due_date.replace(/-/g, '/'));
   if (reminder.due_time) {
     const [ hour, minutes ] = reminder.due_time.split(':').map(_ => parseInt(_));
     const dueDate = reminder.due_date;
