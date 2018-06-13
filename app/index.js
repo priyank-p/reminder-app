1f38253146023ec10fef4f091d8b6380be500829
Priyank Patel <priyankp390@gmail.com>
2018-06-13 20:12:39 +0000

    tracker: clean up and finish up tracker.

--- app/index.js
+++ app/index.js
@@ -27,7 +27,7 @@ const routes = require('./routes');
 
 
 const app = express();
-// const tracker = require('./tracker'); // eslint-disable-line no-useless
+const tracker = require('./tracker'); // eslint-disable-line no-unused-vars
 const STATIC_DIR = path.resolve(__dirname, '../static');
 settings(app);
 app.use('/static', express.static(STATIC_DIR));
