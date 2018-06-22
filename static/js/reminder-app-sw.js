e336afe40dbc6633a13f2fa1fe84e93270c588f1
Priyank Patel <priyankp390@gmail.com>
2018-06-22 21:15:16 +0000

    service-worker: delete the reminder when the notification is clicked.

--- static/js/reminder-app-sw.js
+++ static/js/reminder-app-sw.js
@@ -4,7 +4,7 @@ const version = '{{sw-loader hash}}';
 // the minifier.
 console.log(version);
 
-self.addEventListener('install', function() {
+self.addEventListener('install', function () {
   self.skipWaiting();
 });
 
@@ -12,7 +12,7 @@ self.addEventListener('activate', function (event) {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('push', function(event) {
+self.addEventListener('push', function (event) {
   const { body, title, tag } = event.data.json();
 
   event.waitUntil(
@@ -20,5 +20,21 @@ self.addEventListener('push', function(event) {
       body: body,
       tag: tag
     })
+  );
+});
+
+// we do not import the request module
+// here it just better to use fetch
+function deleteReminder(tag) {
+  const id = parseInt(tag.replace('reminder-', ''));
+  return fetch(`/api/reminders/delete/${id}`, {
+    method: 'DELETE'
+  });
+}
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    deleteReminder(event.notification.tag)
   );
 });
