3a49faa64d3611f4fb8f548c406e141ab2f2dcb7
Priyank Patel <priyankp390@gmail.com>
2018-06-25 20:25:18 +0000

    sw-loader: use hash from webpack-info.js which is faster.

--- tools/sw-loader.js
+++ tools/sw-loader.js
@@ -1,30 +1,13 @@
-const path = require('path');
-const { hashElement: hashDir } = require('folder-hash');
-
-const STATIC_DIR = path.resolve(__dirname, '../static');
-const opts = {
-  files: {
-    include: ['*.js', '*.scss']
-  },
-  folders: {
-    exclude: ['.*', 'templates', 'webpack-bundles']
-  }
-};
+const webpackInfo = require('./webpack-info');
 
 module.exports = function (content, map, meta) {
   const callback = this.async();
-  hashDir(STATIC_DIR, opts)
-    .then((hash) => {
-      content = content.replace(/\{\{sw-loader\shash\}\}/, hash.hash);
-      if (this.query.emitDevFile === true) {
-        // we emit exact copy of sw file, just the version changed
-        // in dev mode, it served from /webpack/sw-dev.js path
-        this.emitFile('sw-dev.js', content, map);
-      }
+  content = content.replace(/\{\{sw-loader\shash\}\}/, webpackInfo.hash);
+  if (this.query.emitDevFile === true) {
+    // we emit exact copy of sw file, just the version changed
+    // in dev mode, it served from /webpack/sw-dev.js path
+    this.emitFile('sw-dev.js', content, map);
+  }
 
-      callback(null, content, map, meta);
-    })
-    .catch(err => {
-      callback(err);
-    });
+  callback(null, content, map, meta);
 };
