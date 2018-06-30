909a41d032e357365e37d04563857d3a292ef80b
Priyank Patel <priyankp390@gmail.com>
2018-06-30 21:14:57 +0000

    scripts: add npm-info script and only install dependencies if needed.

---
+++ scripts/npm-info.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+const packageJSON = require('../package.json');
+
+const cacheFile = path.resolve(__dirname, '../var/cache-hashes');
+let caches;
+if (!fs.existsSync(cacheFile)) {
+  caches = {};
+  fs.writeFileSync(cacheFile, '{}', 'utf8');
+} else {
+  caches = JSON.parse(fs.readFileSync(cacheFile, 'utf8'));
+}
+
+const hashFunction = crypto.createHash('md5');
+hashFunction.update(JSON.stringify(packageJSON.dependencies));
+
+const hash = hashFunction.digest('hex');
+let needToInstall = false;
+
+if (!caches.npm || caches.npm !== hash) {
+  needToInstall = true;
+  caches.npm = hash;
+  fs.writeFileSync(cacheFile, JSON.stringify(caches), 'utf8');
+}
+
+module.exports = {
+  needToInstall
+};
