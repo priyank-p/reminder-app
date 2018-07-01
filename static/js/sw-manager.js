dabae21c58ae353f36a90577b30a635b21af77ab
Priyank Patel <priyankp390@gmail.com>
2018-07-01 16:35:42 -0400

    sw-manager: add a class of .delete-by-sw if deleted by sw.

--- static/js/sw-manager.js
+++ static/js/sw-manager.js
@@ -1,1 +1,15 @@
-export default function SWManager() {}
+import $ from './dom';
+
+function reminderDeleted(id) {
+  const selector = `.reminder[data-id="${id}"]`;
+  const reminder = $(selector);
+  reminder.classList.add('deleted-by-sw');
+}
+
+export default function SWManager(event) {
+  switch (event.data.message) {
+  case 'reminder-deleted':
+    reminderDeleted(event.data.id);
+    break;
+  }
+}
