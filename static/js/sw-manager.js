fbbb7a2d36d84ce9027e8418b037fbeeef5fdba7
Priyank Patel <priyankp390@gmail.com>
2018-07-01 16:41:54 -0400

    sw-manager: change the title if the app was deleted.
    Prepends "(deleted)" in front of the title

--- static/js/sw-manager.js
+++ static/js/sw-manager.js
@@ -4,6 +4,7 @@ function reminderDeleted(id) {
   const selector = `.reminder[data-id="${id}"]`;
   const reminder = $(selector);
   reminder.classList.add('deleted-by-sw');
+  reminder.title = '(deleted) ' + reminder.title;
 }
 
 export default function SWManager(event) {
