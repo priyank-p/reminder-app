efaef27e8c50e76305633ca8db9ceca64734c426
Priyank Patel <priyankp390@gmail.com>
2018-07-13 15:40:01 +0000

    sw-manager.ts: type annotate sw-manager.ts.

--- static/js/sw-manager.ts
+++ static/js/sw-manager.ts
@@ -1,13 +1,13 @@
 import $ from './dom';
 
-function reminderDeleted(id) {
+function reminderDeleted(id: number) {
   const selector = `.reminder[data-id="${id}"]`;
   const reminder = $(selector);
   reminder.classList.add('deleted-by-sw');
   reminder.title = '(deleted) ' + reminder.title;
 }
 
-export default function SWManager(event) {
+export default function SWManager(event: MessageEvent) {
   switch (event.data.message) {
   case 'reminder-deleted':
     reminderDeleted(event.data.id);
