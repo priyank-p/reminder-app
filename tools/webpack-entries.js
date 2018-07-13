f51a8a37c674c50dacc7319c44ba0287586a37e7
Priyank Patel <priyankp390@gmail.com>
2018-07-13 19:34:12 -0400

    webpack-entries: update the filename after rename.

--- tools/webpack-entries.js
+++ tools/webpack-entries.js
@@ -2,7 +2,7 @@ const mainBundle = {
   'main': [
     './static/js/index.js',
     './static/js/app-hotkeys.ts',
-    './static/js/event-listeners.js'
+    './static/js/event-listeners.ts'
   ],
   'main-css': './static/scss/reminder-app.scss',
   'sw': './static/js/reminder-app-sw.js'
