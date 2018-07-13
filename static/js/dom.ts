dfa4a2bb543bb3bb8f7a6a6067fcf3a8b77221c3
Priyank Patel <priyankp390@gmail.com>
2018-07-13 14:44:00 +0000

    dom.ts: add todo for correct return type.

--- static/js/dom.ts
+++ static/js/dom.ts
@@ -1,5 +1,6 @@
 type DomTypeParam = 'node' | 'nodelist';
 
+// TODO: Figure out correct return type for this function
 export default function $(sel: string, type?: DomTypeParam): any {
   const els = document.querySelectorAll(sel);
   if (type === 'nodelist') {
