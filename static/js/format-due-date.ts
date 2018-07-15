dd611ed45edbf80730e167911a92bab18400254f
Priyank Patel <priyankp390@gmail.com>
2018-07-15 18:01:51 -0400

    Fix linting errors in format-due-date.ts.

--- static/js/format-due-date.ts
+++ static/js/format-due-date.ts
@@ -4,16 +4,16 @@ import $ from './dom';
 export default function formatDate() {
   ($('.reminder, .archive', 'nodelist') as Node[]).forEach((el: Element) => {
     const due_date: HTMLElement = el.querySelector('.due-date');
-  
+
     if (due_date.innerText === '') {
       return;
     }
-  
+
     const date = new Date(due_date.innerText);
     let formattedDate: string = dateFormat(date, 'shortDate') + ' ';
     formattedDate += dateFormat(date, 'shortTime');
     due_date.innerText = formattedDate;
   });
-  
+
   ($('.reminders') as Element).classList.remove('hide');
 }
