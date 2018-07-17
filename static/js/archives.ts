7cd2c2c34300f13f55241902c4324dbd71b9d2ea
Priyank Patel <priyankp390@gmail.com>
2018-07-17 17:05:03 -0400

    archive: add restore context menus on archives.

--- static/js/archives.ts
+++ static/js/archives.ts
@@ -1,5 +1,7 @@
 import * as request from './request';
+import { init as initContextMenus } from './context-menu';
 
+initContextMenus();
 function restore(id: string) {
   request.get(`/api/archives/restore/${id}`)
     .then(() => {
