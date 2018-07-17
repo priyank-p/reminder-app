dff5878afd21f92a4d144630b7b697844d8de8ac
Priyank Patel <priyankp390@gmail.com>
2018-07-17 17:20:09 -0400

    archives: implement restore archive feature.

--- static/js/archives.ts
+++ static/js/archives.ts
@@ -1,5 +1,6 @@
 import * as request from './request';
 import { init as initContextMenus } from './context-menu';
+import $ from './dom';
 
 initContextMenus();
 function restore(id: string) {
@@ -8,3 +9,14 @@ function restore(id: string) {
       location.href = '/';
     });
 }
+
+const archives = $('.archives') as Element;
+archives.addEventListener('click', (event: MouseEvent) => {
+  const el: Element = event.target as Element;
+  const archive: HTMLElement = el.parentElement.parentElement.parentElement;
+  const id = archive.dataset.id;
+
+  if (el.classList.contains('restore')) {
+    restore(id);
+  }
+});
