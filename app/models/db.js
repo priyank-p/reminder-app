303fede9b4b4c680fc577a76f4dc9a4eb7db52cc
Priyank Patel <priyankp390@gmail.com>
2018-07-29 15:29:28 +0000

    models: db make sure db name is tests when in tests.

--- app/models/db.js
+++ app/models/db.js
@@ -3,10 +3,15 @@ const Uplevel = require('level-uplevel');
 const env = require('../env');
 
 const { production } = env;
-const DB_NAME = production ? 'reminder-app' : `reminder-app-${env.mode}`;
+let DB_NAME = production ? 'reminder-app' : 'reminder-app-development';
+if (env.tests) {
+  DB_NAME = 'reminder-app-test';
+}
+
 const DB_PATH = path.resolve(__dirname, '../../var/', DB_NAME);
 const db = new Uplevel(DB_PATH);
 
+console.log(DB_NAME);
 // we don't use the async method here,
 // since we know the table is created in advance
 const reminders = new Uplevel.UplevelTableInstance(db, 'reminders');
