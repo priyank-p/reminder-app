7e261beba27d82be29cc8567a1f39866a750f558
Priyank Patel <priyankp390@gmail.com>
2018-07-29 17:45:02 -0400

    request: throw error sent from server instead of blank error.
    This now throw error which looks like `Error: <error>` rather
    than before which was just emoty Error.

--- static/js/request.ts
+++ static/js/request.ts
@@ -2,6 +2,13 @@ const defaultPostHeaders: object = {
   'Content-Type': 'application/json'
 };
 
+function handleError(res: Response): Promise<any> {
+  return res.text()
+    .then(text => {
+      throw new Error(text);
+    });
+}
+
 export function post(url: string, opts?): Promise<Response> {
   if (typeof opts.body === 'object') {
     opts.body = JSON.stringify(opts.body);
@@ -18,7 +25,7 @@ export function post(url: string, opts?): Promise<Response> {
   const request = fetch(url, requestData)
     .then((res) => {
       if (!res.ok) {
-        throw Error();
+        return handleError(res);
       }
 
       return res;
@@ -31,7 +38,7 @@ export function get(url: string | Request, opts?: RequestInit): Promise<Response
   return fetch(url, opts)
     .then(res => {
       if (!res.ok) {
-        throw Error();
+        return handleError(res);
       }
 
       return res;
