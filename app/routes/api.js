f8aca6fe20ed8860949a73c2c03ae8a548d0782f
Priyank Patel <priyankp390@gmail.com>
2018-07-30 10:45:36 -0400

    api-routes: send error code of 400 during error.
    Error code of 400 is much more sutiable for this.

--- app/routes/api.js
+++ app/routes/api.js
@@ -48,13 +48,13 @@ router.delete('/reminders/delete/:id', async (req, res) => {
 router.post('/reminders/update/:id', async (req, res) => {
   const id = +req.params.id;
   if (isNaN(id)) {
-    res.status(500);
+    res.status(400);
     res.send('Reminder id to delete must be passed in, and must be valid integer');
     return;
   }
 
   if (Object.keys(req.body).length !== 2) {
-    res.status(500);
+    res.status(400);
     res.send('Reminder must be passed in!');
     return;
   }
@@ -65,7 +65,7 @@ router.post('/reminders/update/:id', async (req, res) => {
       res.send(`Reminder with id: ${id} was updated!`);
     })
     .catch(() => {
-      res.status(500);
+      res.status(400);
       res.send(`Cannot updated reminder with id: ${id}!`);
     });
 });
@@ -78,7 +78,7 @@ router.get('/archives/all', async (req, res) => {
 router.get('/archives/:id', async (req, res) => {
   const id = +req.params.id;
   if (isNaN(id)) {
-    res.status(500);
+    res.status(400);
     res.send('A valid id must be passed in!');
     return;
   }
@@ -88,14 +88,14 @@ router.get('/archives/:id', async (req, res) => {
       res.json(arhive);
     })
     .catch(() => {
-      res.status(500).send(`No arhives with id: ${id}.`);
+      res.status(400).send(`No arhives with id: ${id}.`);
     });
 });
 
 router.get('/archives/restore/:id', async (req, res) => {
   const id = +req.params.id;
   if (isNaN(id)) {
-    res.status(500).send('Id passed in must be valid!');
+    res.status(400).send('Id passed in must be valid!');
     return;
   }
 
@@ -106,20 +106,20 @@ router.get('/archives/restore/:id', async (req, res) => {
     await archives.deleteArchive(id);
     res.json({ newId });
   } catch(e) {
-    res.status(500).send(`Cannot restore archive with id: ${id}`);
+    res.status(400).send(`Cannot restore archive with id: ${id}`);
   }
 });
 
 router.delete('/archives/delete/:id', async (req, res) => {
   const id = Number(req.params.id);
   if (isNaN(id)) {
-    res.status(500).send('Archive id must be valid!');
+    res.status(400).send('Archive id must be valid!');
   }
 
   archives.deleteArchive(id)
     .then(() => { res.send('Archive deleted.'); })
     .catch(() => {
-      res.status(500).send(`Cannot delete archive with id ${id}.`);
+      res.status(400).send(`Cannot delete archive with id ${id}.`);
     });
 });
 
