163104bbb639f411071aa590b544c6dea6cf9573
Priyank Patel <priyankp390@gmail.com>
2018-08-05 10:05:15 -0400

    scripts: make sure var directory is present.
    This makes sure npm-info.js file does not throw ENONET error
    when var directory is not present.

--- scripts/npm-info.js
+++ scripts/npm-info.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const fsExtra = require('fs-extra');
 const path = require('path');
 const crypto = require('crypto');
 const packageJSON = require('../package.json');
@@ -6,6 +7,10 @@ const packageJSON = require('../package.json');
 const cacheFile = path.resolve(__dirname, '../var/cache-hashes');
 let caches;
 if (!fs.existsSync(cacheFile)) {
+  // we make sure /var directory is avalible
+  const pathInfo = path.parse(cacheFile);
+  fsExtra.emptyDirSync(pathInfo.dir);
+
   caches = {};
   fs.writeFileSync(cacheFile, '{}', 'utf8');
 } else {
