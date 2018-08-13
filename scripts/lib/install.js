d3acbfd57e721d2a38ff794398711d44d2db7e76
Priyank Patel <priyankp390@gmail.com>
2018-08-13 16:57:39 -0400

    scripts: refactor prompt_for_port into lib.

--- scripts/lib/install.js
+++ scripts/lib/install.js
@@ -3,6 +3,7 @@
 */
 
 const path = require('path');
+const inquirer = require('inquirer');
 const run = require('../../tools/run');
 
 // common options for run calls
@@ -26,7 +27,41 @@ async function isPm2Installed() {
   return isInstalled;
 }
 
+async function prompt_for_port(args) {
+  if (args.port) {
+    process.env.PORT = args.port;
+    return;
+  }
+
+  if (args.nonInteractive) {
+    return;
+  }
+
+  const prompts = [{
+    type: 'input',
+    name: 'port',
+    message: 'Select a port for reminder-app to run on',
+    default: process.env.PORT,
+    validate(input) {
+      const port = Number(input);
+      if (isNaN(port)) {
+        throw Error('Port should be a valid number!');
+      }
+
+      if (port < 0 || port > 65536) {
+        throw Error('A valid port is greater than 0 and less than 65536!');
+      }
+
+      return true;
+    }
+  }];
+
+  const response = await inquirer.prompt(prompts);
+  process.env.PORT = response.port;
+}
+
 module.exports = {
   ignore, opts,
-  isPm2Installed
+  isPm2Installed,
+  prompt_for_port
 };
