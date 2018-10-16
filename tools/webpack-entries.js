1203b5698185643fe09620c104e05c4c9dddee29
Priyank Patel <priyankp390@gmail.com>
2018-10-16 18:25:21 +0000

    Remove font-awesome dependecies and webpack bundles.

--- tools/webpack-entries.js
+++ tools/webpack-entries.js
@@ -14,17 +14,8 @@ const mainBundle = {
   'sw': './static/js/reminder-app-sw.js'
 };
 
-const FontAwesomeBasePath = './node_modules/@fortawesome/fontawesome-free-webfonts/css/';
-const vendorBundle = {
-  'vendor': [
-    FontAwesomeBasePath + 'fontawesome.css',
-    FontAwesomeBasePath + 'fa-solid.css'
-  ]
-};
-
 const bundles = {
   ...mainBundle,
-  ...vendorBundle
 };
 
 module.exports = bundles;
