1d2144d3b8626c7edff3be2c3b9a8f53a16aee16
Priyank Patel <priyankp390@gmail.com>
2019-02-16 16:52:50 +0000

    api-events: add client side code for handling api events.

--- static/js/index.ts
+++ static/js/index.ts
@@ -2,6 +2,7 @@ import formatDate from './format-due-date';
 import setupDateEditing from './edit-due-date';
 import register from './register';
 import * as menu from './menu';
+import { apiEvents } from './api-events';
 
 formatDate();
 setupDateEditing();
