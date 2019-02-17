0e2ee94b828c3af1298b7a10c925f660475ed021
Priyank Patel <priyankp390@gmail.com>
2019-02-17 00:31:50 +0000

    app: implement real-time update for UI when reminder is deleted.

--- static/js/index.ts
+++ static/js/index.ts
@@ -3,10 +3,18 @@ import setupDateEditing from './edit-due-date';
 import register from './register';
 import * as menu from './menu';
 import { apiEvents } from './api-events';
+import $ from './dom';
 
 formatDate();
 setupDateEditing();
 register();
+
+apiEvents.on('reminder-deleted', (id) => {
+  const $reminder = $(`.reminder[data-id="${id}"]`);
+  if ($reminder) {
+    $reminder.parentElement.removeChild($reminder);
+  }
+});
 
 menu.on('archive', () => {
   location.href = '/archives';
